8d53bf44199a40b48593a858c89d5238275b6fc3
Charlles Abreu <abreu@eq.ufrj.br>
2023-01-27 11:19:35 -0300

    Fixed imports

--- cvlib/__init__.py
+++ cvlib/__init__.py
@@ -2,4 +2,7 @@
 
 # Add imports here
 from ._version import __version__  # noqa: F401
-from .cvlib import Angle, Distance, RadiusOfGyration, Torsion
+from .cvlib import Angle  # noqa: F401
+from .cvlib import Distance  # noqa: F401
+from .cvlib import RadiusOfGyration  # noqa: F401
+from .cvlib import Torsion  # noqa: F401
