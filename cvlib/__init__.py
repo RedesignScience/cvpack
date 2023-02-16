a8feb5a3704857bc3ef6358e6ba6921283fb78d3
Charlles Abreu <abreu@eq.ufrj.br>
2023-02-16 12:16:06 -0300

    Solved isort/black incompatibility

--- cvlib/__init__.py
+++ cvlib/__init__.py
@@ -2,4 +2,10 @@
 
 # Add imports here
 from ._version import __version__  # noqa: F401
-from .cvlib import Angle, Distance, NumberOfContacts, RadiusOfGyration, Torsion  # noqa: F401
+from .cvlib import (  # noqa: F401
+    Angle,
+    Distance,
+    NumberOfContacts,
+    RadiusOfGyration,
+    Torsion,
+)
