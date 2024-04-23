220a471d021f2b36ace8a379948421e134c46809
Charlles Abreu <craabreu@gmail.com>
2024-04-23 19:00:23 -0400

    Turned path into subpackage

---
+++ cvpack/path/__init__.py
@@ -0,0 +1,7 @@
+"""
+.. package:: path
+    :platform: Linux, MacOS, Windows
+    :synopsis: Specifications for Path Collective Variables
+"""
+
+from .path import Metric, deviation, progress  # noqa: F401
