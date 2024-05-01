Fixes bug in effective mass calculation (#98)
* Fixed broken links

* Added description to conda recipe

* Buf fix

cvpack/utils.py
@@ -179,7 +179,7 @@ def compute_effective_mass(
     squared_forces = np.sum(np.square(force_vectors), axis=1)
     nonzeros = np.nonzero(squared_forces)[0]
     if nonzeros.size == 0:
-        return mmunit.Quantity(np.inf, force._mass_unit)
+        return np.inf
     mass_values = np.fromiter(map(get_mass, nonzeros), dtype=np.float64)
     return 1.0 / np.sum(squared_forces[nonzeros] / mass_values)
 